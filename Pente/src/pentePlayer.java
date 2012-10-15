680a50f7089c961fa9ad2db222ca1e741f407214
CothX <coth_x@yahoo.com>
2012-10-15 12:06:27 -0500

    player may be finished at this point, state needs charcount needs to be changed to count our board and toMove needs to be corrected to return the correct player

---
+++ Pente/src/pentePlayer.java
@@ -0,0 +1,27 @@
+import jaima.game.Player;
+
+public class pentePlayer extends Player{
+
+char stone;
+    
+    /**
+     * Choose a mark
+     * @param m W or B
+     */
+    public pentePlayer(char s)
+    {
+        stone = Character.toUpperCase(s);
+    }
+    
+    public pentePlayer(String s)
+    {
+        stone = s.toUpperCase().charAt(0);
+    }
+    
+    public String toString()
+    {
+        return stone + "";
+    }
+	
+	
+}
