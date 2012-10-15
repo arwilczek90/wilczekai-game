53c1e660f16873a8a76c5a5985175a94e6fcd5f2
CothX <coth_x@yahoo.com>
2012-10-15 11:39:48 -0500

    This message relates to changes in penteState

---
+++ Pente/src/movePlayer.java
@@ -0,0 +1,11 @@
+import jaima.game.Player;
+
+public class movePlayer extends Player{
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Pente/src/penteMove.java
@@ -0,0 +1,11 @@
+import jaima.game.Move;
+
+public class penteMove extends Move{
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
